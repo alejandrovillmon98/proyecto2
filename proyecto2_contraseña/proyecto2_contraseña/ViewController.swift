17f4d5cb61a8ed0ec2e3fc9435b3772fce646382
Alejandro villanueva M <alejandrovillmon@gmail.com>
2018-10-11 15:26:46 -0500

    Add files via upload

---
+++ proyecto2_contraseña/proyecto2_contraseña/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proyecto2_contraseña
+//
+//  Created by Macbook on 08/10/18.
+//  Copyright © 2018 iosLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var Usuario: UITextField!
+    @IBOutlet weak var Contraseña: UITextField!
+    
+    @IBAction func Ingresar(_ sender: UIButton) {
+        
+    
+    }
+    
+}
+
